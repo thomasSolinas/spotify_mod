dae44d1d13c6204209dae12048f31f0acbcacc58
thomasSolinas <solinas.thomas06@gmail.com>
2026-03-03 18:20:36 +0100

    feat: working paywall removal on mini player resize

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,39 @@
+// src/core/config.ts
+var MOD_NAME = "spotify-mod";
+var MODS = {
+  MINI_PLAYER: "miniplayer",
+  ADS: "ads"
+};
+
+// src/mods/miniplayer/paywallRemover.ts
+var MOD_PREFIX = `[${MOD_NAME}:${MODS.MINI_PLAYER}]`;
+var PLAYER_SELECTOR = '[data-testid="pip-hover-element"]';
+function removePaywall(pipWindow) {
+  const observer = new MutationObserver((mutations) => {
+    const player = pipWindow.document.querySelector(PLAYER_SELECTOR);
+    if (player) {
+      console.log(`${MOD_PREFIX} Found player element.`, player);
+      const paywall = player.previousSibling;
+      if (paywall) {
+        paywall.remove();
+        console.log(`${MOD_PREFIX} Paywall removed. 
+                    \n Resizing PiP window is now possible!`);
+        observer.disconnect();
+      }
+    }
+  });
+  observer.observe(pipWindow.document.body, {
+    childList: true,
+    subtree: true
+  });
+}
+
 // src/mods/miniplayer/index.ts
 function initMiniPlayer() {
   window.documentPictureInPicture.addEventListener("enter", (event) => {
     const pipWindow = event.window;
     console.log("PiP opened!", pipWindow);
+    removePaywall(pipWindow);
   });
 }
 
--- src/mods/miniplayer/paywallRemover.ts
+++ src/mods/miniplayer/paywallRemover.ts
@@ -1,20 +1,25 @@
 
 import { MOD_NAME, MODS } from "../../core/config";
 
+const MOD_PREFIX = `[${MOD_NAME}:${MODS.MINI_PLAYER}]`;
 
-const paywallSelector = 'data-testid="pip-hover-element"'; 
+const PLAYER_SELECTOR = '[data-testid="pip-hover-element"]'; // Paywall is immediate sibling before this element
 
 export function removePaywall(pipWindow: Window) {
-    const observer = new MutationObserver((mutations: MutationRecord[]) => {
+    const observer = new MutationObserver((mutations: MutationRecord[]) => { // Check for mutations in the PiP window
 
+        const player: HTMLElement | null = pipWindow.document.querySelector(PLAYER_SELECTOR);
+        if (player) {
+            console.log(`${MOD_PREFIX} Found player element`, player);
 
-        const paywall = pipWindow.document.querySelector(paywallSelector);
-        if (paywall) {
-            paywall.remove();
-            observer.disconnect(); // stop watching once removed
-            console.log(`[${MOD_NAME}:${MODS.MINI_PLAYER}] Paywall removed`);
+            const paywall = player.previousSibling;
+            if (paywall) {
+                paywall.remove();
+                console.log(`${MOD_PREFIX} Paywall removed. Resizing PiP window is now possible!`);
+
+                observer.disconnect(); // stop watching once removed
+            }
         }
-
 
         // // Look for the paywall element in the added nodes
         // for (const mutation of mutations) {
