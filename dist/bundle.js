3100d977b4a9fb6ab657bcc25396cad8e2ebe007
thomasSolinas <solinas.thomas06@gmail.com>
2026-03-03 17:57:41 +0100

    fix: index.ts not calling removePaywall

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,24 +1,8 @@
-// src/mods/miniplayer/paywallRemover.ts
-function removePaywall(pipWindow) {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes) {
-        console.log("node added to PiP:", node);
-      }
-    }
-  });
-  observer.observe(pipWindow.document.body, {
-    childList: true,
-    subtree: true
-  });
-}
-
 // src/mods/miniplayer/index.ts
 function initMiniPlayer() {
   window.documentPictureInPicture.addEventListener("enter", (event) => {
     const pipWindow = event.window;
     console.log("PiP opened!", pipWindow);
-    removePaywall(pipWindow);
   });
 }
 
--- src/mods/miniplayer/index.ts
+++ src/mods/miniplayer/index.ts
@@ -1,8 +1,11 @@
+
+
 import { removePaywall } from './paywallRemover';
 
 export function initMiniPlayer() {
     window.documentPictureInPicture.addEventListener('enter', (event: DocumentPictureInPictureEvent) => {
         const pipWindow = (event).window;
         console.log('PiP opened!', pipWindow);
+        removePaywall(pipWindow);
     });
 }
