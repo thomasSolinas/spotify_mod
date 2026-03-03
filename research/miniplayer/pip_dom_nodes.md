747a5f23a0b47a7a1266c7166607d564c57638ea
thomasSolinas <solinas.thomas06@gmail.com>
2026-03-03 17:38:56 +0100

    Notes added

---
+++ research/miniplayer/pip_dom_nodes.md
@@ -0,0 +1,7 @@
+# PiP nodes
+
+- **a.html**  --> first node: style
+- **b.html** --> second node: main html
+    `data-testid="pip-hover-element"` --> overlay paywall to remove
+- **c.html** --> third node: progress bar
+
--- src/mods/miniplayer/paywallRemover.ts
+++ src/mods/miniplayer/paywallRemover.ts
@@ -4,6 +4,7 @@ export function removePaywall(pipWindow: Window) {
             for (const node of mutation.addedNodes) {
                 console.log('node added to PiP:', node);
                 // paywall removal logic will go here once we identify the element
+                
             }
         }
     });
