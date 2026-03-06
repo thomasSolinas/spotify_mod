a00bedb704d55a830547604cd6aaf93f9a88c552
thomasSolinas <solinas.thomas06@gmail.com>
2026-03-06 16:52:09 +0100

    add miniPlayerSetCustom (added size)

--- src/mods/miniplayer/index.ts
+++ src/mods/miniplayer/index.ts
@@ -1,11 +1,16 @@
 import { removePaywall } from './paywallRemover';
 import { MINI_PLAYER_PREFIX } from './miniPlayer_config';
+import { setCustomMiniPlayerSize } from './miniPlayerSetCustom';
 
 
 export function initMiniPlayerMod() {
     window.documentPictureInPicture.addEventListener('enter', (event: DocumentPictureInPictureEvent) => {
         const pipWindow = (event).window;
+
         console.log(`${MINI_PLAYER_PREFIX} PiP opened!`, pipWindow);
-        removePaywall(pipWindow);
+
+        removePaywall(pipWindow);// remove paywall
+
+        setCustomMiniPlayerSize(pipWindow); // set custom size (optional)
     });
 }
---
+++ src/mods/miniplayer/miniPlayerSetCustom.ts
@@ -0,0 +1,8 @@
+//sets size of mini player, default is 1x1 (it auto sets to min size, so it will be as small as possible)
+
+export function setCustomMiniPlayerSize(pipWindow: Window, width = 1, height = 1) {
+    pipWindow.resizeTo(width, height);
+}
+
+// make exported function that accepts an object (i'll define what it contains in src/typc/spotify.d.ts) 
+// object is used to set custom preferences for the mini player, such as size, position, etc. (for now just size)
