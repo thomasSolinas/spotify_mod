f1eb2d590eec2a1b1bbee8405f8f0f1cb77f89de
thomasSolinas <solinas.thomas06@gmail.com>
2026-04-21 11:10:53 +0200

    feat: add PLAYER_SELECTOR to config

--- src/mods/miniplayer/miniPlayer_config.ts
+++ src/mods/miniplayer/miniPlayer_config.ts
@@ -1,3 +1,4 @@
 import { MOD_NAME, MODS } from "../../core/config";
 
 export const MINI_PLAYER_PREFIX = `[${MOD_NAME}:${MODS.MINI_PLAYER}]`;
+export const PLAYER_SELECTOR = '[data-testid="pip-hover-element"]';
